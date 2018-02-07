f07a4cd568d9ae82e3e65fd9fa42752fa9bf52ca
April Buell <adb701@gmail.com>
2018-02-07 15:21:54 -0700

    final

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'Zxy87d9qTlfjrEtpCRAjKqEmx',
+  consumer_secret: '0KeH8hUO2m8yIwt1b7WHJFg90Sj3McZ9Wfbp8EiuCGbwZg8b4W',
+  access_token_key: '961047816532516864-Pg8KCigz4RGNGOwdb2VapFJscMmiIdY',
+  access_token_secret: 'dKxLntKJbm1PSq0FCIpHUsSvRaZHxLnPpLj1LlM2DRbjv',
+}
+
+module.exports = twitterKeys;
